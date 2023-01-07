ea3a33bbba40f4956827b773d7e131a87e980219
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-01-08 00:22:28 +0530

    Updated Projects part

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,14 +8,14 @@ const AboutContent = () => {
     <div className="about">
          <div className="left">
           <h1>About me...</h1>
-          <p>I am a 3rd year student in NIT Durgapur pursuing ECE. I have prior experience in the backend and frontend technologies including React, Node, MongoDB, Express, Python, Javascript, Django, MySQL, Bootstrap and sound knowledge of C++, Object Oriented Programming, Azure, Cloud Computing. I have build projects using the technologies mentioned above and currently learning more about Computer Networking, DBMS and Operating Systems. I love to build applications which can tackle real world problems.</p>
+          <p>I am a 3rd year student in <a href="https://nitdgp.ac.in/">NIT Durgapur</a> pursuing ECE.I have prior experience in the backend and frontend technologies including React, Node, MongoDB, Express, Python, Javascript, Django, MySQL, Bootstrap and sound knowledge of C++, Object Oriented Programming, Azure, Cloud Computing. I have build projects using the technologies mentioned above and currently learning more about Computer Networking, DBMS and Operating Systems. I love to build applications which can tackle real world problems.</p>
           <div>
           <Link to="/contact" className="btn contact-btn">
             Contact me
           </Link>
           </div>
           <div>
-          <a href="https://mantine.dev/others/carousel/"><button className="btn btn-resume">My CV</button></a>
+          <a href="https://mantine.dev/others/carousel/"><button className="btn btn-resume">Resume</button></a>
           </div>
         </div>
 

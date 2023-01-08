fff615eb0dabb66a055fb8f9a64b23d6603c112a
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-01-09 00:54:06 +0530

    Updated the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,11 @@
 import "./Card.css";
-import React from 'react'
+import React from 'react';
+import {GrNode} from "react-icons/gr";
+import {SiExpress} from "react-icons/si";
+import {SiMongodb} from "react-icons/si";
+import {FaReact} from "react-icons/fa";
+import {AiFillHtml5} from "react-icons/ai";
+
 
 
 const Card = (props) => {
@@ -12,7 +18,9 @@ const Card = (props) => {
         <p>This app allows users to share their secrets with anonymous names of their particular type and authorizes them to sign in via OAuth2.0 .</p>
         <p>Tech Stack Used</p>
         <div className="icons-container">
-        Icons Used
+        <GrNode size={30} style={{color:"white"}}/>
+        <SiExpress size={30} style={{color:"white"}}/>
+        <SiMongodb size={30} style={{color:"white"}}/>
         </div>
         <div className="active-buttons">
         <a href="#" className="btn-card">SOURCE CODE</a>
@@ -26,7 +34,8 @@ const Card = (props) => {
         <p>This application allows users to manage and view their expenses list while keeping a track of them. I represents them in the bar chart form</p>
         <p>Tech Stack Used</p>
         <div className="icons-container">
-          Icons Used
+        <GrNode size={30} style={{color:"white"}}/>
+        <FaReact size={30} style={{color:"white"}}/>
         </div>
         <div className="active-buttons">
         <a href="#" className="btn-card">SOURCE CODE</a>
@@ -40,7 +49,9 @@ const Card = (props) => {
         <p>Users can plan their priorities and set them in correct order to stay consistent and focussed throughout the day avoiding distractions.</p>
         <p>Tech Stack Used</p>
         <div className="icons-container">
-          Icons Used
+        <GrNode size={30} style={{color:"white"}}/>
+        <SiExpress size={30} style={{color:"white"}}/>
+        <SiMongodb size={30} style={{color:"white"}}/>
         </div>
         <div className="active-buttons">
         <a href="#" className="btn-card">SOURCE CODE</a>
@@ -53,7 +64,7 @@ const Card = (props) => {
         <h2>Simon Game</h2>
         <p>Users can start interacting it with the help of keystrokes and play the game until they follow the rules of the game. Each time a sound is played while a button is pressed.</p>
         <div className="icons-container">
-          Icons Used
+        <AiFillHtml5 size={30} style={{color:"white"}}/>
         </div>
         <div className="active-buttons">
         <a href="#" className="btn-card">SOURCE CODE</a>

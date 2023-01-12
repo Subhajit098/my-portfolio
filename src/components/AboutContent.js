c8ed0efe8c25c1d451903d56388a7e8edc13c5d9
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-01-12 12:33:31 +0530

    Modified About

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -6,29 +6,47 @@ import myImg from "../assets/cropped.png";
 const AboutContent = () => {
   return (
     <div className="about">
-         <div className="left">
-          <h1>About me...</h1>
-          <p>I am a 3rd year student in <a href="https://nitdgp.ac.in/">NIT Durgapur</a> pursuing ECE.I have prior experience in the backend and frontend technologies including React, Node, MongoDB, Express, Python, Javascript, Django, MySQL, Bootstrap and sound knowledge of C++, Object Oriented Programming, Azure, Cloud Computing. I have build projects using the technologies mentioned above and currently learning more about Computer Networking, DBMS and Operating Systems. I love to build applications which can tackle real world problems.</p>
-          <div>
-          <Link to="/contact" className="btn contact-btn">
-            Contact me
-          </Link>
-          </div>
-          <div>
-          <a href="https://mantine.dev/others/carousel/"><button className="btn btn-resume">Resume</button></a>
-          </div>
+        <div className="left">
+          LEFT
         </div>
 
         <div className="right">
-          <div className="img-container">
-              <img src={myImg} className="img" alt="My-Pic"></img>
-          </div>
+          RIGHT
         </div>
     </div>
   )
 }
 
 export default AboutContent
+
+
+
+
+
+
+
+
+
+
+{/* <div className="left">
+<div className="img-container">
+     <img src={myImg} className="img" alt="My-Pic"></img>
+ </div>
+ <h1>About me...</h1>
+ <p>I am a 3rd year student in <a href="https://nitdgp.ac.in/">NIT Durgapur</a> pursuing ECE.I have prior experience in the backend and frontend technologies including React, Node, MongoDB, Express, Python, Javascript, Django, MySQL, Bootstrap and sound knowledge of C++, Object Oriented Programming, Azure, Cloud Computing. I have build projects using the technologies mentioned above and currently learning more about Computer Networking, DBMS and Operating Systems. I love to build applications which can tackle real world problems.</p>
+ <div>
+ <Link to="/contact" className="btn contact-btn">
+   Contact me
+ </Link>
+ </div>
+ <div>
+ <a href="https://mantine.dev/others/carousel/"><button className="btn btn-resume">Resume</button></a>
+ </div>
+</div>
+
+<div className="right">
+ 
+</div> */}
 
 
 

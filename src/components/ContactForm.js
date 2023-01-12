3ca511454916c49b32758b0a75b6b84e937c534c
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-01-13 01:50:57 +0530

    Added React Lazy

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,8 @@
 import "./ContactForm.css";
 import React from 'react'
 import styled from "styled-components";
+const LazyMap = React.lazy(()=> import("./Location"));
+
 
 const ContactForm = () => {
 
@@ -10,7 +12,11 @@ const ContactForm = () => {
     <Wrapper className="section">
         <h2 className="contact-heading">Connect with me</h2>
 
-        <iframe title="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.775541021324!2d87.69668821490264!3d23.64820818463983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f9c4aae0409665%3A0xa90b8a0471d1eecb!2sKashimbazar%20Durga%20Mandir!5e0!3m2!1sen!2sin!4v1672685459680!5m2!1sen!2sin" width="100%" height="300" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        <React.Suspense fallback={<h1 style={{color:"white"}}>Loading...</h1>}>
+              <LazyMap/>
+        </React.Suspense>
+
         <div className="container">
 
               <div className="container-flex form-container-flex">

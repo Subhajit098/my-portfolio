117e8190a5d85762dbbaf5faa98aaff8882d3063
Subhajit Dey <sd.20u10020@btech.nitdgp.ac.in>
2023-05-23 11:56:23 +0530

    Update RenderProjects.js

--- src/components/RenderProjects.js
+++ src/components/RenderProjects.js
@@ -1,7 +1,6 @@
 import  "./RenderProjects.css";
 import React from 'react'
 import "./Card";
-import { Carousel } from '@mantine/carousel';
 import Card from "./Card";
 
 const RenderProjects = () => {
